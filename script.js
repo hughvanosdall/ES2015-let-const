154e5d7a55782d4fa06ca9bdf6300f583300e7af
Hugh Vanosdall <hughvanosdall@gmail.com>
2022-08-09 00:36:12 -0700

    fix

---
+++ script.js
@@ -0,0 +1,21 @@
+const PI = 3.14;
+PI = 42;
+
+// What is the difference between var and let?
+
+// you can redeclare var, but not let
+
+// What is the difference between var and const?
+
+// you cannot redeclare or reassign const
+
+// What is the difference between let and const?
+
+// you cannot reassign const
+
+// What is hoisting?
+
+// It’s the way that we explain how the JS compiler works.
+// Variables are lifting or “hoisted” to the top of the scope they are declared in.
+// When using var, you can access the variable name and it’s undefined value before it is used later on.
+// Function declarations are also hoisted and can be invoked “before” they are defined in a codebase.
